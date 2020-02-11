1f1ee9f914d40145a9e9be6892d88b17434dab19
Ryan <ryangalaxymini@gmail.com>
2020-02-11 17:54:35 -0300

    Imagem da logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,29 @@
+import 'dart:io' as Io;
 
+import 'package:flutter/material.dart';
+
+void main() => runApp(Myapp());
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return (MaterialApp(
+      title: 'The Bruxo Tales',
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Image(
+                image: AssetImage('assets/60.jpg'),
+                width: 315,
+                height: 160,
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+}
