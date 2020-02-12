b79313ecfca96484437a3adbcf22dde8527456ba
unknown <juniorbertran@softeam.com.br>
2020-02-12 01:41:07 -0300

    origin

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\junior\Documents\Downloads\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\junior\Desktop\lul\ProjetoPessoal\TheBruxoTales\thebruxotales"
+export "FLUTTER_APPLICATION_PATH=C:\Users\junior\Desktop\lul\ProjetoPessoal\TheBruxoTales\TheBruxoTales"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
