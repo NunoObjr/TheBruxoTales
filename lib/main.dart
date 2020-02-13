26c983ff3198d63bc921b95e68d46145bffe84b4
Andrey Rezende <Andrey Rezende>
2020-02-13 13:01:53 -0300

    origin

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,28 @@
-
+import 'package:flutter/material.dart';
+void buttoncriar(){
+  runApp(MaterialApp(
+      home: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                  width: 180,
+                  child: RaisedButton(
+                      child: Text("Criar",
+                          style: TextStyle(
+                            fontSize: 45,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black,
+                            letterSpacing: 2.0,
+                            fontStyle: FontStyle.normal,
+                          )),
+                      onPressed: () {},
+                      color: Colors.green[300],
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0))))
+            ],
+          ))));
+}
+void main() {
+  buttoncriar();
+}
