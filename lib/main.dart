e183e14b7ab5a29b4535aa5b12c44a83347308ba
Andrey Rezende <Andrey Rezende>
2020-02-18 22:18:44 -0300

    origin

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,42 @@
 import 'package:flutter/material.dart';
 
-void buttoncriar(x){
-  runApp(MaterialApp(
-      home: Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                  width: 180,
-                  child: RaisedButton(
-                      child: Text('$x',
-                          style: TextStyle(
-                            fontSize: 45,
-                            fontWeight: FontWeight.w400,
-                            color: Colors.black,
-                            letterSpacing: 2.0,
-                            fontStyle: FontStyle.normal,
-                          )),
-                      onPressed: () {},
-                      color: Colors.green[300],
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15.0))))
-            ],
-          ))));
+Widget buttonCriar(texto) {
+  return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black38,
+                blurRadius: 6,
+                spreadRadius: -2,
+                offset: Offset(0, 4)),
+          ]),
+      width: 180,
+      child: RaisedButton(
+          onPressed: () {},
+          child: Text('$texto',
+              style: TextStyle(
+                fontSize: 45,
+                fontWeight: FontWeight.w400,
+                color: Colors.black,
+                letterSpacing: 2.0,
+                fontStyle: FontStyle.normal,
+              )),
+          color: Color.fromARGB(1000, 121, 153, 127),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0))));
 }
+
+Widget input(x) {
+  return (TextField(
+    obscureText: false, //texto em formato de senha (true para sim)
+    decoration: InputDecoration(
+      border: OutlineInputBorder(),
+      labelText: '$x', /*,labelStyle caso queira editar o labelText?*/
+    ),
+  ));
+}
+
 void main() {
-  var x = 'Exclua a variavel x obertran(sei q voce está lendo';
-  buttoncriar(x);
+  
 }
