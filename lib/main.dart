5f63981a899e095ecb0a429f8aa344940da1138a
unknown <juniorbertran@softeam.com.br>
2020-02-26 17:24:35 -0300

    origin

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  var criar = 'Criar';
-  var login = 'Login';
+  const criar = 'Criar';
+  const login = 'Login';
   
   runApp(MaterialApp(
     title: 'teste',
@@ -13,7 +13,7 @@ void main() {
       children: <Widget>[
         Padding(
           padding: EdgeInsets.only(top:20.0),
-          child: build(),
+          child: logo(),
         ),
         Padding(
           padding: EdgeInsets.only(top:60.0),
@@ -29,50 +29,50 @@ void main() {
   ));
 }
 
-Widget buttonCriar(texto){
-  return new Container(
-    child: 
-    SizedBox(
-                  width: 180,
-                  child: RaisedButton(
-                      child: Text('$texto',
-                            style: TextStyle(
-                            fontSize: 45,
-                            fontWeight: FontWeight.w400,
-                            color: Colors.black,
-                            letterSpacing: 2.0,
-                            fontStyle: FontStyle.normal,
-                          )),
-                      onPressed: () {},
-                      color: Colors.green[300],
-                      shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0))))
-  );
-}
-Widget buttonGo(){
-  return new Container(
-    child:
-    RaisedButton(
-      child: Text('GO',
-      ),
-      onPressed: () {},
-    )
-  );
-}
-
-  Widget build() {
-    return new Container(
-          child: 
-              Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: AspectRatio(
-                    aspectRatio: 2.5 / 1,
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.all(Radius.circular(40.0)),
-                        child: Image(
-                          image: AssetImage('assets/witcher.jpg'),
-                          fit: BoxFit.fill,
-                        )),
-                  )),
+    Widget buttonCriar(texto){
+      return new Container(
+        child: 
+        SizedBox(
+                      width: 180,
+                      child: RaisedButton(
+                          child: Text('$texto',
+                                style: TextStyle(
+                                fontSize: 45,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black,
+                                letterSpacing: 2.0,
+                                fontStyle: FontStyle.normal,
+                              )),
+                          onPressed: () {},
+                          color: Colors.green[300],
+                          shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0))))
       );
-  }
+      }
+      Widget buttonGo(){
+        return new Container(
+          child:
+          RaisedButton(
+            child: Text('GO',
+            ),
+            onPressed: () {},
+          )
+        );
+      }
+
+      Widget logo() {
+        return new Container(
+              child: 
+                  Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: AspectRatio(
+                        aspectRatio: 2.5 / 1,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.all(Radius.circular(40.0)),
+                            child: Image(
+                              image: AssetImage('assets/witcher.jpg'),
+                              fit: BoxFit.fill,
+                            )),
+                      )),
+          );
+      }
