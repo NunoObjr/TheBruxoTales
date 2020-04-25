a8327133631fdedc5758a10457f7ed177cd2df34
Nunoobjr <obertran.lima@fasminvestimentos.com.br>
2020-04-24 22:10:33 -0300

    origin

--- lib/screens/appHome.dart
+++ lib/screens/appHome.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import '../my_flutter_app_icons.dart';
 
 class AppHome extends StatefulWidget {
   @override
@@ -7,6 +9,7 @@ class AppHome extends StatefulWidget {
 
 class _AppHomeState extends State<AppHome> {
   @override
+  
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
@@ -18,19 +21,112 @@ class _AppHomeState extends State<AppHome> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Padding(
-                padding: EdgeInsets.only(top: 30, left: 30, right: 70),
+                padding: EdgeInsets.only(top: 30, left: 10),
                 child: searchBar()),
             bio(),
             ],
             )),
-          Text('teste3'),
+            profileBar()
         ])
       );
   }
+
+
+
+Widget texto(texto){
+  return Text('$texto', style: TextStyle(fontSize: 20),);
 }
 
-Widget texto(texto) {
-  return Text('$texto', style: TextStyle(fontSize: 20),);
+Widget profileBar() {
+  var opacidade = 0.0;
+  var clique = false;
+  setState((){});
+  return Container(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top:25),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(5),
+                child:OutlineButton(onPressed: (){
+                setState(() {
+                  clique = !clique;
+                });
+                if(clique)
+                  setState(() {
+                    opacidade=1.0;
+                  });
+                else
+                  setState(() {
+                    opacidade = 0.0;
+                  });
+                print(opacidade);
+                }, borderSide: BorderSide.none,child:Icon(Icons.account_circle, size: 65)),
+                
+              ),
+              Padding(
+                padding: EdgeInsets.all(5),
+                child: Text("CarpeadoBugado", style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),)
+              )
+            ]
+          )
+        ),
+        Opacity(opacity:opacidade,child:Padding(
+          padding: EdgeInsets.only(top:10,left: 4),
+          child: Row(
+            children: <Widget>[
+              Icon(Icons.settings, size: 45,),
+              Padding(padding:EdgeInsets.only(left:20),child:Icon(Icons.launch, size: 45))
+            ]
+          )
+        )),
+        Opacity(opacity: opacidade,child:Padding(
+          padding: EdgeInsets.only(top:12,left: 4),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[600],
+              borderRadius: BorderRadius.circular(20)
+            ),
+            child:Row(children: <Widget>[Padding(padding:EdgeInsets.only(left:5),child:Icon(Icons.people_outline, size: 45)),Padding(child:SizedBox(width:60,child:Text("Amigos",style: TextStyle(fontWeight: FontWeight.bold),)),padding: EdgeInsets.only(left:8),)],)
+          ),
+        )),
+        Opacity(opacity: opacidade,child:Padding(
+          padding: EdgeInsets.only(top:12,left: 4),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[600],
+              borderRadius: BorderRadius.circular(20)
+            ),
+            child:Row(children: <Widget>[Padding(padding:EdgeInsets.only(left:5),child:Icon(Icons.history, size: 45)),Padding(child:SizedBox(width:60,child:Text("Historico",style: TextStyle(fontWeight: FontWeight.bold),)),padding: EdgeInsets.only(left:8),)],)
+          ),
+        )),
+        Opacity(opacity: opacidade,child:Padding(
+          padding: EdgeInsets.only(top:12,left: 4),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[600],
+              borderRadius: BorderRadius.circular(20)
+            ),
+            child:Row(children: <Widget>[Padding(padding:EdgeInsets.only(left:5),child:Icon(Icons.layers, size: 45)),Padding(child:SizedBox(width:60,child:Text("Galeria",style: TextStyle(fontWeight: FontWeight.bold),)),padding: EdgeInsets.only(left:8),)],)
+          ),
+        )),
+        Opacity(opacity: opacidade,child:Padding(
+          padding: EdgeInsets.only(top:12,left: 4),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[600],
+              borderRadius: BorderRadius.circular(20)
+            ),
+            child:Row(children: <Widget>[Padding(padding:EdgeInsets.only(left:5),child:Icon(Icons.help_outline, size: 45)),Padding(child:SizedBox(width:60,child:Text("Dicas",style: TextStyle(fontWeight: FontWeight.bold),)),padding: EdgeInsets.only(left:8),)],)
+          ),
+        ))
+      ],
+    ),
+  );
 }
 
 Widget bio(){
@@ -40,19 +136,19 @@ Widget bio(){
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         Padding(
-              padding: EdgeInsets.only(top: 60, left: 45),
+              padding: EdgeInsets.only(top: 60, left: 30),
               child: texto('CarpeadoBugado'),
             ),
             Padding(
-              padding: EdgeInsets.only(top: 10, left: 45),
+              padding: EdgeInsets.only(top: 10, left: 30),
               child: texto('Pontos: 570'),
             ),
             Padding(
-              padding: EdgeInsets.only(top: 10, left: 45),
+              padding: EdgeInsets.only(top: 10, left: 30),
               child: texto('Ranking global: 1'),
             ),
             Padding(
-              padding: EdgeInsets.only(top: 10, left: 45),
+              padding: EdgeInsets.only(top: 10, left: 30),
               child: texto('Ranking Brasil: 1'),
             ),
       ]
@@ -160,4 +256,5 @@ Widget searchBar(){
                         fillColor: Colors.white,
                       ),
                     ));
+}
 }
